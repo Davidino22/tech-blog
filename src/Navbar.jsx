54a4c3588fd65a654f1b24221ef13ecca72d3589
David Pecic <david-pecic@gmx.de>
2023-07-19 14:11:13 +0200

    changed style and added a hamburge icon for the navigation in mobile form

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useUserContext } from './UserProvider';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 
 
@@ -25,19 +26,21 @@ function Navbar() {
       <Link to="/"><div className='absolute left-6 top-2 text-2xl hover:cursor-pointer '>Tech-Blog</div></Link>
 
 
-      <div className=" NavLinks flex flex-row md:flex-col absolute top-10 ">
-        <div className="Links flex flex-row absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2  text-2xl justify-between w-60  " >
+      <div className=" NavLinks  bg-pink-100 flex flex-col absolute top-10 right-0 p-4 md:flex md:static md:flex-row   md:text-2xl  ">
+        <div className="Links
+        flex flex-col
+           text-2xl  md:flex-row md:top-1/2 md:transform md:-translate-x-1/2 md:-translate-y-1/2 md:absolute  md:justify-between md:left-1/2 md:w-60 " >
           <div className='hover:cursor-pointer '>About</div>
-          <div className='hover:cursor-pointer '>Profile</div>
-          <Link to="/new">New</Link>
+          <div className='hover:cursor-pointer' >Profile</div>
+          <Link to="/new" >New</Link>
         </div>
-        <div className='buttons flex flex-row absolute right-8 top-1 gap-6  '>
+        <div className='buttons flex-col gap-y-2 flex md:flex-row md:absolute  md:right-3 md:top-1/2  md:-translate-y-1/2  '>
 
           {/* check if someone is loged in if yes then show us a logout button */}
           {user === null ?
-            <> <Link to="/login" ><button className='bg-blue-400 px-4 rounded-md py-2 text-white'>SignIn</button></Link>
-              <Link to="/register" ><button className='bg-blue-400 px-4 rounded-md py-2 text-white'> Register</button></Link> </> :
-            <button className='bg-blue-400 px-4 rounded-md py-2 text-white font-mono' onClick={logout}>Log out </button>}
+            <> <Link to="/login" ><button className='bg-blue-400 px-2 rounded-md py-1 text-white text-xl mx-2'>SignIn</button></Link>
+              <Link to="/register" ><button className='bg-blue-400 px-2 rounded-md py-1 text-white text-xl mx-2'> Register</button></Link> </> :
+            <button className='bg-blue-400 px-2 rounded-md py-1 text-white font-mono text-xl  ' onClick={logout}>Log out </button>}
 
         </div>
 
@@ -45,7 +48,7 @@ function Navbar() {
       </div>
 
 
-
+      <GiHamburgerMenu className='md:hidden absolute right-6 top-2 ' size={30} />
     </div>
   )
 }
--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+
+
 // Modul for the postcard
 
 
@@ -14,11 +16,11 @@ import { Link } from 'react-router-dom'
 export default function PostCard(props) {
   const { title, content, id } = props
   return (
-    <Link to={`/posts/${id}`} className="w-3/5 h-40 my-4 hover:scale-105 transition ease-in-out shadow-md hover:shadow-xl"  >
+    <Link to={`/posts/${id}`} className="w-3/5 min-h-40 my-4 hover:scale-105 transition ease-in-out shadow-md hover:shadow-xl"  >
 
       <div className='bg-indigo-100  w-full h-full rounded-lg  '>
         <p className='text-2xl  border-b-4 border-slate-800'>{title}</p>
-        <p className='pt-4'>{content}</p>
+        <p className='pt-4 h-full'>{content}</p>
       </div>
 
 
