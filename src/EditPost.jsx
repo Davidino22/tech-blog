0c7b3080231dd87dc38143f0786ff7e0ad99824c
David Pecic <david-pecic@gmx.de>
2023-09-27 14:30:17 +0200

    finished editing functinality

--- src/EditPost.jsx
+++ src/EditPost.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 import { useState, useEffect } from 'react'
 
 
@@ -7,7 +7,9 @@ import { useState, useEffect } from 'react'
 function EditPost() {
   let { id } = useParams();
   const [post, setPost] = useState(null)
-  const [post2, setPost2] = useState({ title: "misa ide u skolu", content: "misa dobio jedan" })
+  const navigate = useNavigate();
+
+
 
   useEffect(() => {
     getPost()
@@ -34,6 +36,30 @@ function EditPost() {
 
 
 
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+
+    const response = await fetch(`${url}/posts/${id}`, {
+
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json"
+      },
+      body: JSON.stringify(
+        post
+      ),
+    })
+
+    const data = await response.json()
+    console.log(data)
+    navigate('/')
+
+  }
+
+
+
+
 
 
 
@@ -45,11 +71,11 @@ function EditPost() {
     < div className="h-screen bg-gradient-to-tl from-red-500 to-blue-500  flex justify-center align-center ">
       {post &&
 
-        <form className='flex flex-col justify-center align-center '>
+        <form className='flex flex-col justify-center align-center ' onSubmit={handleSubmit}>
 
-          <input className='bg-blue-500 w-80 h-30' value={post2.title} onChange={(e) => setPost2({ title: e.target.value, ...post2 })} />
-          <textarea className='bg-yellow-500 w-60 h-60' value={post2.content} onChange={(e) => setPost2({ content: e.target.value, ...post2 })} />
-          <button className='bg-blue-300 text-white'>edit</button>
+          <input className='bg-blue-500 w-80 h-30' value={post.title} onChange={(e) => setPost({ ...post, title: e.target.value, })} />
+          <textarea className='bg-yellow-500 w-60 h-60' value={post.content} onChange={(e) => setPost({ ...post, content: e.target.value })} />
+          <button className='bg-blue-300 text-white' >edit</button>
 
         </form>}
 
